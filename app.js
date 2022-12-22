207e8dc55ce57764a1190cb4d229237caf4d0ea9
Felipe D <felipedomingosps@gmail.com>
2022-12-22 07:34:17 -0300

    Add input to toggle between 'Read' and 'Not read yet'

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const library = []
-
+/* FUNCTIONS */
 function Book (title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read ? "Read" : "Not read yet",
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 }
 
 function addBookToLibrary (title, author, pages, read) {
-    library.push(new Book(title, author, pages, read))
+    library.push(new Book(title, author, pages, read));
 }
 
-function addInitialBooks(){
+function addInitialBooks (){
     const books = [
         {
             title:'The Hobbit',
@@ -34,30 +33,38 @@ function addInitialBooks(){
             pages: 234,
             read: true
         }
-    ]
+    ];
 
-    books.forEach(book => addBookToLibrary(book.title, book.author, book.pages, book.read))
+    books.forEach(book => addBookToLibrary(book.title, book.author, book.pages, book.read));
 }
 
-addInitialBooks()
-
-function createBookCard(book, index){
+function createBookCard (book, index){
     return `<article class="library__book" data-index="${index}">
     <h2 class="book__title">${book.title}</h2>
     <p class="book__author">${book.author}</p>
     <p class="book__pages">${book.pages}</p>
-    <p class="book__read">${book.read}</p>
-    <button class="book__remove-button"></button>
-</article>`
+    <div class="book__read><p class="read__status">${book.read}</p><input type="range" min="1" max="2" value="${book.read === "Read" ? 2 : 1}" class="read__toggle">
+    <button class="book__remove-button">X</button>
+</article>`;
 }
 
-function displayBookCard(bookCard) {
-    const sectionLibrary = document.querySelector('.library')
-    sectionLibrary.innerHTML += bookCard
+function displayBookCard (bookCard) {
+    const sectionLibrary = document.querySelector('.library');
+    sectionLibrary.innerHTML += bookCard;
 }
 
-function displayAllBooks() {
+function displayAllBooks () {
     library.forEach((book, index)=> {
         displayBookCard(createBookCard(book,index))
-    })
+    });
 }
+
+/* APP */
+
+const library = [];
+
+addInitialBooks();
+
+displayAllBooks();
+
+
